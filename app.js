1d19c6abe4210a86a35eed8634741d4bc309bed7
jobscale <jobscale@jsx.jp>
2019-07-23 16:52:37 +0900

    make it better

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const http = require("http").Server(app);
 // require the socket.io module
 const io = require("socket.io");
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 //bodyparser middleware
 app.use(bodyParser.json());
